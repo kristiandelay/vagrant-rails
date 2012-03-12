6dc74b0d249e843a3b2d94a8bdabc321408a0435
kristian delay <kristiandelay@gmail.com>
2012-03-11 20:32:08 -0700

    annotate party

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  email      :string(255)
+#  password   :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class User < ActiveRecord::Base
 end
